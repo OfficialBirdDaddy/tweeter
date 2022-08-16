197d77589b38196df4c07fd7dde8771564b213b0
Alexander Jackson <alexx.j.jackson@gmail.com>
2022-08-16 23:37:06 +0000

    add length of tweet countdown

---
+++ public/scripts/composer-char-counter.js
@@ -0,0 +1,15 @@
+$(document).ready(function() {
+
+  const maxChars = 140;
+  $('#tweet-text').keyup(function() {
+    
+    const charNumber = $(this).val().length;
+    
+    $('.counter').text(maxChars - charNumber);
+
+    if (charNumber > maxChars) { //changes colour to red if over max chararacters
+      $('.counter').css('color', 'red');
+    }
+
+  })
+});
