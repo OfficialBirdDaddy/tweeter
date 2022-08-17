227a2623c9df71e1f3f493add6d37bb55b5ddea2
Alexander Jackson <alexx.j.jackson@gmail.com>
2022-08-17 21:34:36 +0000

    begin work on tweet element creation

--- public/scripts/client.js
+++ public/scripts/client.js
@@ -4,3 +4,27 @@
  * Reminder: Use (and do all your DOM work in) jQuery's document ready function
  */
 
+// Function to create a tweet element using predefined HTML elements
+
+const createTweetElement = (tweetObj) => { 
+  console.log(tweetObj);
+  const $tweet = $('<article>').addClass('tweet');
+
+  const $img = $('<img>').attr('src', tweetObj.user.avatars);
+  const $name = $('<h3>').text(tweetObj.user.name);
+  const $username = $('<h4>').text(tweetObj.user.username);
+
+  const $div = $('<div>').append($img).append($name);
+  const $h4 = $('<h4>').append($username);
+
+  const $header = $('<header>').append($div).append($h4);
+
+  $tweet.append($header);
+
+  const $tweetContent = $('<p>').text(tweetObj.content.text);
+  const $div2 = $('<div>').addClass('tweet-body').append($tweetContent);
+
+  $tweet.append($div2);
+
+  
+}
