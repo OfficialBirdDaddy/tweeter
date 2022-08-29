33807084c78e57fd2d9478c2cf3cda818112f502
Alexander Jackson <alexx.j.jackson@gmail.com>
2022-08-29 19:43:26 +0000

    fix bug where first character did not lower counter

--- public/scripts/composer-char-counter.js
+++ public/scripts/composer-char-counter.js
@@ -1,16 +1,18 @@
 $(document).ready(function() {
 
   const maxChars = 140;
-  $('#tweet-text').on("keydown", function() {
+  $('#tweet-text').on("input", function() {
     
-    const charNumber = $(this).val().length;
+    const charNumber = ($(this).val().length);
     
     $('.counter').text(maxChars - charNumber);
 
     if (charNumber > maxChars) { //changes colour to red if over max chararacters
-      $('.counter').css('color', 'red');
-    } else {
-      $('.counter').css('color', 'black');
+      $('.counter').addClass('alert');
+    }
+
+    if (charNumber <= maxChars) { //changes colour back to black if under max characters
+      $('.counter').removeClass('alert');
     }
 
   })
